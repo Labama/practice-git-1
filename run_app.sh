3bebc258857851c50cbf6210c36eb7ec5e382ba9
Labama <55188444+Labama@users.noreply.github.com>
2021-08-18 15:17:41 +0300

    Create run_app.sh

---
+++ run_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo su
+apt update 
+apt install -fy python-setuptools build-essential python-dev python-pip build-essential wget mc htop
+pip install flask psutil
+wget https://gist.githubusercontent.com/Nklya/4f8944d99e43b6c44a9767a93daa1592/raw/149ee890f4201ac5ce53888846f6b3e7601be6ba/app.py -O app.py
+FLASK_APP=app.py flask run --host=0.0.0.0 --port=80
+
+
+
+while true; do (curl http://34.140.75.54/); sleep 5; done
