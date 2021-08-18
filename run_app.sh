3f1b8cb5dfbd101cb3820f8b635ab4574e69717d
Labama <55188444+Labama@users.noreply.github.com>
2021-08-18 16:20:38 +0300

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -6,6 +6,3 @@ pip install flask psutil
 wget https://gist.githubusercontent.com/Nklya/4f8944d99e43b6c44a9767a93daa1592/raw/149ee890f4201ac5ce53888846f6b3e7601be6ba/app.py -O app.py
 FLASK_APP=app.py flask run --host=0.0.0.0 --port=80
 
-
-
-while true; do (curl http://34.140.75.54/); sleep 5; done
